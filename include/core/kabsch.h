5a36ccb9846637d8a08b70c06059ac906608396f
Amr ALHOSSARY <amr_alhossary@hotmail.com>
2024-05-09 16:45:43 -0400

    Added a parameter `secondAlreadyCentered` to kabsch.h
    It should enhance the performance a little.
    Also included some corections

--- include/core/kabsch.h
+++ include/core/kabsch.h
@@ -4,24 +4,26 @@
 #define KABSCH_H
 
 #include <Eigen/Geometry>
-#include <iostream> //FIXME remove this import line later
+#include "core/KEnRef.h"
 
 #define VERBOSE false
 
 // Given two sets of 3D points, find the rotation + translation + scale
-// which best maps the first set to the second.
+// which best transforms the first set to the second (i.e. if applied to the first will result in the second).
+//The first and second are also called in and out or before and after.
 // Source: http://en.wikipedia.org/wiki/Kabsch_algorithm
 // original code obtained from
 // https://github.com/oleg-alexandrov/projects/blob/master/eigen/Kabsch.cpp
-
 // The input 3D points are stored as rows (every point in a row).
-template <typename precision>
-class Kabsch{
+template<typename precision>
+class Kabsch {
     //TODO check whether there is an available implementation in Eigen.
     //TODO you can check here https://eigen.tuxfamily.org/dox/group__TutorialLinearAlgebra.html
     //TODO and https://eigen.tuxfamily.org/dox/group__TutorialGeometry.html
 public:
-    static Eigen::Transform<precision,3,Eigen::Affine> Find3DAffineTransform(const CoordsMatrixType<precision>& p, const CoordsMatrixType<precision>& q) {
+    static Eigen::Transform<precision, 3, Eigen::Affine>
+    Find3DAffineTransform(const CoordsMatrixType<precision> &p, const CoordsMatrixType<precision> &q,
+                          bool secondAlreadyCentered = false) {
 
         // Default output
 		Eigen::Transform<precision,3,Eigen::Affine> A;
@@ -47,19 +49,25 @@ public:
 		precision scale = dist_q/dist_p;
 		q_temp /= scale;
 
-		// Find the centroids then shift to the origin
-		Eigen::Vector3<precision> p_ctr = Eigen::Vector3<precision>::Zero();
-		Eigen::Vector3<precision> q_ctr = Eigen::Vector3<precision>::Zero();
-		for (int row = 0; row < p_temp.rows(); row++) {
-			p_ctr += p_temp.row(row);
-			q_ctr += q_temp.row(row);
-		}
-		p_ctr /= p_temp.rows();
-		q_ctr /= q_temp.rows();
-		for (int row = 0; row < p_temp.rows(); row++) {
-			p_temp.row(row) -= p_ctr;
-			q_temp.row(row) -= q_ctr;
-		}
+        // Find the centroids then shift to the origin
+        Eigen::Vector3<precision> p_ctr = Eigen::Vector3<precision>::Zero();
+        Eigen::Vector3<precision> q_ctr = Eigen::Vector3<precision>::Zero();
+        for (int row = 0; row < p_temp.rows(); row++) {
+            p_ctr += p_temp.row(row);
+        }
+        p_ctr /= p_temp.rows();
+        for (int row = 0; row < p_temp.rows(); row++) {
+            p_temp.row(row) -= p_ctr;
+        }
+        if (! secondAlreadyCentered){
+            for (int row = 0; row < p_temp.rows(); row++) {
+                q_ctr += q_temp.row(row);
+            }
+            q_ctr /= q_temp.rows();
+            for (int row = 0; row < p_temp.rows(); row++) {
+                q_temp.row(row) -= q_ctr;
+            }
+        }
 
 		// SVD
 		//  Eigen::MatrixXf Cov = p_temp * q_temp.transpose();
@@ -71,15 +79,15 @@ public:
 			std::cout << "Matrix V " << std::endl << svd.matrixV() << std::endl << std::endl;
 #endif
 
-		// Find the rotation
-		float d = (svd.matrixV() * svd.matrixU().transpose()).determinant();
-		if (d > 0)
-			d = 1.0;
-		else
-			d = -1.0;
-		Eigen::Matrix3<precision> I = Eigen::Matrix3<precision>::Identity(3, 3);
-		I(2, 2) = d;
-		Eigen::Matrix3<precision> R = svd.matrixV() * I * svd.matrixU().transpose();
+        // Find the rotation
+        precision d = (svd.matrixV() * svd.matrixU().transpose()).determinant();
+        if (d > 0)
+            d = 1.0;
+        else
+            d = -1.0;
+        Eigen::Matrix3<precision> I = Eigen::Matrix3<precision>::Identity(3, 3);
+        I(2, 2) = d;
+        Eigen::Matrix3<precision> R = svd.matrixV() * I * svd.matrixU().transpose();
 
         // The final transform
         A.linear() = scale * R;
