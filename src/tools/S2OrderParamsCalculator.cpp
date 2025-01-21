684dd751353b8c9426c7359cd60c8af7c19a1c5d
Amr ALHOSSARY <amr_alhossary@hotmail.com>
2025-01-21 06:50:56 -0500

    Updated s2calc
    updated parameters & their parsing
    removed s2r

--- src/tools/S2OrderParamsCalculator.cpp
+++ src/tools/S2OrderParamsCalculator.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <iomanip>
-#include "mpi.h"
+#include <unistd.h>
+#include "gromacs/fileio/xtcio.h"
+#include "gromacs/utility/smalloc.h"
+#include "gromacs/math/vecdump.h"
+#include "gromacs/gmxlib/network.h"
+#include "gromacs/mdlib/gmx_omp_nthreads.h"
+#include "gromacs/utility.h"
+#include "gromacs/pbcutil/pbc.h"
+#include "gromacs/domdec/domdec_struct.h"
+#include "gromacs/domdec/ga2la.h"
+#include "core/kabsch.h"
+#include "gmxinterface/KEnRefForceProvider.h"
+#include "gmxinterface/KEnRefMDModule.h"
 #include "core/KEnRef.h"
 #include "core/IoUtils.h"
 #include "core/kabsch.h"
@@ -39,24 +51,26 @@ public:
         const std::string GUIDE_C_ALPHA = IoUtils::getEnvParam("S2_GUIDE_C_ALPHA" ,"guideC-alpha");
         const std::string INDEX_FILE_LOCATION = IoUtils::getEnvParam("S2_INDEX_FILE_LOCATION", "../res/10nsstart+fitting/KEnRefAtomIndex.ndx");
         const std::string REFERENCE_FILENAME = IoUtils::getEnvParam("S2_REFERENCE_FILENAME", "../res/10nsstart+fitting/00ns.pdb");
-        const std::string inPath = IoUtils::getEnvParam("S2_IN_PATH", "/smithlab/home/aalhossary/temp/test");
-        const std::string outPath = IoUtils::getEnvParam("S2_OUT_PATH", "/smithlab/home/aalhossary/temp/s2-order-params");
+        const std::string inPathTemplate = IoUtils::getEnvParam("S2_IN_PATH", "/smithlab/home/aalhossary/temp/temp_pdb/data${dataDir}_structure${structure}");
+        const std::string outPathTemplate = IoUtils::getEnvParam("S2_OUT_PATH", "/smithlab/home/aalhossary/temp/s2-order-params");
         //const std::string experimentalDataFileName = IoUtils::getEnvParam("S2_experimentalDataFileName", "../res/google_tests/singleton_data_10nsstart+fit_0+10.csv");
         //const std::string experimentalDataFileName = IoUtils::getEnvParam("S2_experimentalDataFileName", "../res/10nsstart+fitting/singleton_data_10nsstart+fit_3-5_1977pairs_all.csv");
         //const std::string experimentalDataFileName = IoUtils::getEnvParam("S2_experimentalDataFileName", "../res/10nsstart+fitting/singleton_data_10nsstart+fit_0+10_3-5_1812pairs_all.csv");
         //const std::string experimentalDataFileName = IoUtils::getEnvParam("S2_experimentalDataFileName", "../res/10nsstart+fitting/singleton_data_10nsstart+fit_0+10_3-5_1812pairs_80_A.csv");
         const std::string experimentalDataFileName = IoUtils::getEnvParam("S2_experimentalDataFileName", "../res/10nsstart+fitting/singleton_data_10nsstart+fit_0+10_3-5_1814pairs_80_A.csv");
-        const std::string fileNameTemplate = IoUtils::getEnvParam("S2_fileNameTemplate", "data${dataDir}_structure${structure}_repl_${repl}_KEnRef_multi_${K}_.25_${max}_${variant}_${trial}.${frame}.pdb");
+        const std::string fileNameTemplate = IoUtils::getEnvParam("S2_fileNameTemplate", "repl_${repl}_KEnRef_multi_${K}_.25_${max}_${variant}_${trial}.${frame}.pdb");
         const std::string s2OutFileTemplate = IoUtils::getEnvParam("S2_OUTPUT_FILE", "S2_data${dataDir}_structure${structure}_KEnRef_multi_${K}_.25_${max}_${variant}_${trial}.csv");
-        const std::string s2rOutFileTemplate = IoUtils::getEnvParam("S2R_OUTPUT_FILE", "S2r_data${dataDir}_structure${structure}_KEnRef_multi_${K}_.25_${max}_${variant}_${trial}.csv");
+//        const std::string s2rOutFileTemplate = IoUtils::getEnvParam("S2R_OUTPUT_FILE", "S2r_data${dataDir}_structure${structure}_KEnRef_multi_${K}_.25_${max}_${variant}_${trial}.csv");
 
-        std::string dataDir = IoUtils::getEnvParam("S2_DATA_DIR", "0+10");
-        std::string structure = IoUtils::getEnvParam("S2_STRUCTURE", "0+10");
-        std::string k = IoUtils::getEnvParam("S2_K", "1e9");
-        std::string max = IoUtils::getEnvParam("S2_MAX", "800");
-        std::string subSet = IoUtils::getEnvParam("S2_SUBSET", "80");
-        std::string variant = IoUtils::getEnvParam("S2_VARIANT", "A");
-        int trial = IoUtils::getEnvParam("S2_TRIAL", 2);
+        const std::string dataDir = IoUtils::getEnvParam("S2_DATA_DIR", "alef+baaa");
+        const std::string structure = IoUtils::getEnvParam("S2_STRUCTURE", "alef+baaa");
+        const std::string k = IoUtils::getEnvParam("S2_K", "1e9");
+        const std::string max = IoUtils::getEnvParam("S2_MAX", "800");
+//        const std::string subSet = IoUtils::getEnvParam("S2_SUBSET", "80");
+        const int subSet = IoUtils::getEnvParam("S2_SUBSET_", 80);
+        const std::string n = IoUtils::getEnvParam("S2_N", ".25");
+        const std::string variant = IoUtils::getEnvParam("S2_VARIANT", "A");
+        const int trial = IoUtils::getEnvParam("S2_TRIAL", 2);
         const int MAX_FRAME = IoUtils::getEnvParam("S2_MAX_FRAME", 1000);
 
         //Guide atom indices
@@ -126,34 +140,43 @@ public:
             subAtomIdPairs.emplace_back(global0Id_to_sub0Id[atom1], global0Id_to_sub0Id[atom2]);
         }
 
-        std::map<std::string, std::string> replacements{
-                {"dataDir",   dataDir},
-                {"structure", structure},
-                {"K",         k},
-                {"max",       max},
-                {"subSet",    subSet},
-                {"variant",   variant},
-                {"trial",     std::to_string(trial)},
-        };
-        std::string s2OutputPathName = constructFileNamePath(outPath, s2OutFileTemplate, replacements);
-        std::string s2rOutputPathName = constructFileNamePath(outPath, s2rOutFileTemplate, replacements);
+        std::unordered_map<std::string, std::string> replacements{};
+//                {"dataDir",   dataDir},
+//                {"structure", structure},
+//                {"K",         k},
+//                {"N",         n},
+//                {"max",       max},
+//                {"subSet",    subSet}, //TODO FIXME Avoiding a strange bug
+//                {"variant",   variant},
+//                {"trial",     std::to_string(trial)}
+//        };
+        replacements["dataDir"] = dataDir;
+        replacements["subSet"] = std::to_string(subSet);
+        replacements["structure"] = structure;
+        replacements["K"] = k;
+        replacements["N"] = n;
+        replacements["variant"] = variant;
+        replacements["max"] = max;
+        replacements["trial"] = std::to_string(trial);
+        std::string s2OutputPathName = constructFileNamePath(outPathTemplate, s2OutFileTemplate, replacements);
+//        std::string s2rOutputPathName = constructFileNamePath(outPathTemplate, s2rOutFileTemplate, replacements);
         std::cout << "S2 output file path: " << s2OutputPathName << std::endl;
-        std::cout << "S2r output file path: " << s2rOutputPathName << std::endl;
-        std::ofstream s2OutFileStream, s2rOutFileStream;
+//        std::cout << "S2r output file path: " << s2rOutputPathName << std::endl;
+        std::ofstream s2OutFileStream/*, s2rOutFileStream*/;
         s2OutFileStream.open(s2OutputPathName);
-        s2rOutFileStream.open(s2rOutputPathName);
+//        s2rOutFileStream.open(s2rOutputPathName);
         if(s2OutFileStream.is_open()){
             std::cout << "S2 Output file open successfully\n";
         }else{
             std::cerr << "FATAL ERROR: Can't open file [" << s2OutputPathName << "] for writing.\n";
             exit(-1);
         }
-        if(s2rOutFileStream.is_open()){
-            std::cout << "S2r Output file open successfully\n";
-        }else{
-            std::cerr << "FATAL ERROR: Can't open file [" << s2rOutputPathName << "] for writing.\n";
-            exit(-1);
-        }
+//        if(s2rOutFileStream.is_open()){
+//            std::cout << "S2r Output file open successfully\n";
+//        }else{
+//            std::cerr << "FATAL ERROR: Can't open file [" << s2rOutputPathName << "] for writing.\n" << std::endl;
+//            exit(-1);
+//        }
 
 
 //        //Calculate reference atoms (to use to calculate reference S2 order params later)
@@ -182,7 +205,7 @@ public:
                         replacements.insert_or_assign(node.first, node.second);
                     }
 
-                    std::string currentModelPathName = constructFileNamePath(inPath, fileNameTemplate, replacements);
+                    std::string currentModelPathName = constructFileNamePath(inPathTemplate, fileNameTemplate, replacements);
 
                     //calculate guideAtomsX_ZEROIndexed (every model every frame)
                     auto modelAllAtomCoordsMap = IoUtils::getAtomMappingFromPdb<int, Eigen::RowVector3<KEnRef_Real_t>>(
@@ -197,18 +220,8 @@ public:
                     const auto &affineForS2 = Kabsch_Umeyama<KEnRef_Real_t>::find3DAffineTransform(guideAtomsX_ZEROIndexed, guideAtomsReferenceCoords, false, true);
 
                     //and calculate subAtomsXAfterTransform
-                    CoordsMatrixType<KEnRef_Real_t> subAtomsXAfterTransform;
-                    {
-                        CoordsMatrixType<KEnRef_Real_t> subAtomsX = IoUtils::extractCoords(subAtoms0Ids, false,
-                                                                                           modelAllAtomCoordsMap, true);
-                        if (modelIdx == 0) {
-                            Eigen::RowVector3<KEnRef_Real_t> center_of_mass = guideAtomsX_ZEROIndexed.colwise().mean();
-                            subAtomsXAfterTransform = subAtomsX.rowwise() - center_of_mass;
-                        } else
-                            subAtomsXAfterTransform = Kabsch_Umeyama<KEnRef_Real_t>::applyTransform(affineForS2, subAtomsX);
-                    }
-
-                    allSimulationsSubAtomsXVector.at(modelIdx) = subAtomsXAfterTransform;
+                    CoordsMatrixType<KEnRef_Real_t> subAtomsX = IoUtils::extractCoords(subAtoms0Ids, false, modelAllAtomCoordsMap, true);
+                    allSimulationsSubAtomsXVector.at(modelIdx) = Kabsch_Umeyama<KEnRef_Real_t>::applyTransform(affineForS2, subAtomsX);
                 } // end of for(model)
 
                 const auto &frameS2OrderParams = KEnRef<KEnRef_Real_t>::s2OrderParams(allSimulationsSubAtomsXVector, subAtomIdPairs, 0);
@@ -225,39 +238,45 @@ public:
 //                std::cout << "referenceS2OrderParams = " << referenceS2OrderParams.transpose() << std::endl;
 //                std::cout << "difference = " << (frameS2OrderParams - referenceS2OrderParams).transpose() << std::endl;
 //            }
-                s2OutFileStream << frame << ", " << frameS2OrderParams.transpose().format(insideCsvLineFormat) << "\n";
+                s2OutFileStream << frame << ", " << frameS2OrderParams.transpose().format(insideCsvLineFormat) << std::endl;
 //                s2rOutFileStream << frame << ", " << correlationCoefficient << "\n";
             }
             s2OutFileStream.close();
-            s2rOutFileStream.close();
+//            s2rOutFileStream.close();
 //            return correlationCoefficient;
         } catch (const std::runtime_error& e) {
             s2OutFileStream.close();
-            s2rOutFileStream.close();
+//            s2rOutFileStream.close();
             throw e;
         }
     }
 
-    static std::string constructFileNamePath(const std::string &path, const std::string &fileNameTemplate,
-                                             const std::map<std::string, std::string> &replacements) {
-        std::string inputFileName = fileNameTemplate;
-        for (const auto &pair: replacements) {
-            std::regex placeholder("\\$\\{" + pair.first + "\\}");
-            inputFileName = std::regex_replace(inputFileName, placeholder, pair.second);
-        }
-
-        std::string currentModelPathName = path;
+    static std::string constructFileNamePath(const std::string &pathTemplate, const std::string &fileNameTemplate,
+                                             const std::unordered_map<std::string, std::string> &replacements) {
+        std::string currentModelPathName = pathTemplate;
         if (currentModelPathName.back() != '/')
             currentModelPathName.append("/");
-        currentModelPathName.append(inputFileName);
+        currentModelPathName.append(fileNameTemplate);
 //        std::cout << currentModelPathName << std::endl;
 
+        for (const auto &pair: replacements) {
+            std::string str = "\\$\\{"+ pair.first + "\\}";
+//            std::string str = ""; //"\\$\\{";
+//            str.append(pair.first);
+            std::regex placeholder(str);
+            currentModelPathName = std::regex_replace(currentModelPathName, placeholder, pair.second);
+        }
         return currentModelPathName;
     }
 };
 
 int main(int argc, char** argv){
-    MPI_Init(&argc, &argv);
+    if (argc >= 2 && std::strcmp(argv[1], "debug") == 0) {
+        volatile bool holdToDebug = true;
+        while (holdToDebug) {
+            sleep(1);
+        }
+    }
     S2OrderParamsCalculator<KEnRef_Real_t>::calc();
 }
 
